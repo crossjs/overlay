a5f2582c99d102069f09470a7ced0a9894176261
crossjs <one@ffully.com>
2014-04-25 17:32:12 +0800

    init

---
+++ readme.md
@@ -0,0 +1,23 @@
+#overlay
+
+[![Build Status](https://api.travis-ci.org/crossjs/overlay.png?branch=master)](http://travis-ci.org/crossjs/overlay)
+[![Coverage Status](https://coveralls.io/repos/crossjs/overlay/badge.png?branch=master)](https://coveralls.io/r/crossjs/overlay?branch=master)
+
+ > overlay, provides postioning, seajs module
+
+##how to demo
+
+1. checkout
+1. run `npm install`
+1. run `spm install`
+1. run `grunt`
+1. view files in `/demo`
+
+##how to use
+
+1. run `spm install pandora/overlay`
+1. write `require('pandora/overlay/VERSION.NUMBER/overlay')`
+
+##history
+
+- 1.0.0 - release
---
+++ src/overlay.js
@@ -0,0 +1,172 @@
+define(function (require, exports, module) {
+
+/**
+ * 浮动层
+ * @module Overlay
+ */
+
+'use strict';
+
+var $ = require('$'),
+  Widget = require('widget');
+
+/**
+ * Overlay
+ *
+ * @class Overlay
+ * @constructor
+ */
+var Overlay = Widget.extend({
+
+  defaults: {
+    // baseElement: null,
+    // 原点及位置
+    baseXY: {
+      x: 0.5,
+      y: 0.5
+    },
+    // 样式前缀
+    classPrefix: 'ue-overlay',
+    // 事件代理
+    // delegates: { },
+    // 对话框显示隐藏时的动画效果
+    effect: 'fade',
+    height: 'auto',
+    offset: {
+      x: 0,
+      y: 0
+    },
+    position: 'absolute',
+    selfXY: {
+      // 对话框相对于原点位置的位移像素值
+      x: 0.5,
+      y: 0.5
+    },
+    // 浮动层触发点
+    // trigger: null,
+    width: 'auto',
+    zIndex: 901
+  },
+
+  setup: function () {
+    var self = this;
+
+    // element
+    self.element
+      .attr({
+        tabIndex: -1
+      })
+      .css({
+        position: self.option('position'),
+        zIndex: self.option('zIndex'),
+        width: self.option('width'),
+        height: self.option('height'),
+        visibility: 'hidden'
+      });
+
+    self.option('trigger') || self.render();
+  },
+
+  /**
+   * 设定位置（left 与 top）
+   *
+   * @method setPosition
+   */
+  setPosition: function () {
+    var self = this,
+      baseElement = $(self.option('baseElement') || self.viewport),
+      baseXY = self.option('baseXY'),
+      selfElement = self.element,
+      selfXY = self.option('selfXY'),
+      fixed = self.option('position') === 'fixed',
+      offset = baseElement.offset() || { left: 0, top: 0 },
+      left = offset.left + baseElement.outerWidth() * baseXY.x -
+        selfElement.outerWidth() * selfXY.x +
+        (fixed ? 0 : baseElement.scrollLeft()) + offset.x,
+      top = offset.top + baseElement.outerHeight() * baseXY.y -
+        selfElement.outerHeight() * selfXY.y +
+        (fixed ? 0 : baseElement.scrollTop()) + offset.y;
+
+    selfElement.css({
+        left: Math.max(left, 0),
+        top: Math.max(top, 0)
+      });
+
+    return self;
+  },
+
+  /**
+   * 显示对话框
+   *
+   * @method show
+   */
+  show: function () {
+    Overlay.EFFECT[this.option('effect')].show.call(this);
+
+    return this;
+  },
+
+  /**
+   * 隐藏对话框
+   *
+   * @method hide
+   */
+  hide: function () {
+    Overlay.EFFECT[this.option('effect')].hide.call(this);
+
+    return this;
+  },
+
+  render: function () {
+    var self = this;
+
+    Overlay.superclass.render.apply(self);
+
+    // 设置位置
+    self.setPosition();
+
+    self.element.css({
+      visibility: 'visible'
+    });
+
+    return self;
+  }
+
+});
+
+// Overlay.STATE = {
+//   INITIAL: -1,
+//   READY: 0,
+//   VISIBLE: 1,
+//   HIDDEN: 2
+// };
+
+Overlay.EFFECT = {
+
+  none: {
+    show: function () {
+      this.element.show();
+    },
+    hide: function () {
+      this.element.hide();
+    }
+  },
+
+  // 对话框显示隐藏时的动画效果
+  fade: {
+    show: function () {
+      this.element.fadeIn(200, function () {
+        $(this).show();
+      });
+    },
+    hide: function () {
+      this.element.fadeOut(200, function () {
+        $(this).hide();
+      });
+    }
+  }
+};
+
+module.exports = Overlay;
+
+});
